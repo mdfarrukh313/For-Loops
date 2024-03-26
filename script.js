b1f988d896a53d7925193da9a8592b1b95a958dc
mdfarrukh313 <mdfarrukh313@gmail.com>
2024-03-26 14:28:17 +0500

    for loops js and html file created

---
+++ script.js
@@ -0,0 +1,26 @@
+
+for (let i = 1; i <= 10; i++) {
+     console.log(i);
+    
+}
+
+for (let i = 2; i <= 20; i += 2) {
+    console.log(i);
+}
+
+
+for (let i = 1; i <= 15; i += 2) {
+    console.log(i);
+}
+
+
+
+
+let tableNumber = prompt("enter a table number")
+for (let i = 1; i<=10; i++) {
+  document.write(tableNumber + 'x' + i + '=' + tableNumber * i +'<br>')
+    
+}
+
+
+
